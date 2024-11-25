c4f1c61470fd0ef1e8301e27e8b84985bbc6cde3
jaehyuk7 <jahyuck13@naver.com>
2024-11-25 10:42:34 +0900

    feat:생성까지

--- src/main/java/com/example/demo/article/controller/ApiV1ArticleController.java
+++ src/main/java/com/example/demo/article/controller/ApiV1ArticleController.java
@@ -1,8 +1,13 @@
 package com.example.demo.article.controller;
 
+import com.example.demo.article.dto.ArticleDTO;
+import com.example.demo.article.entity.Article;
 import com.example.demo.article.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/articles")
@@ -11,17 +16,36 @@ public class ApiV1ArticleController {
     private final ArticleService articleService;
 
     @GetMapping("")
-    public String list() {
-        return "목록";
+    public List<ArticleDTO> list() {
+        List<ArticleDTO> articleList = new ArrayList<>();
+
+        Article article1 = new Article("제목 1", "내용 1");
+        articleList.add(new ArticleDTO(article1));
+
+        Article article2 = new Article("제목 2", "내용 2");
+        articleList.add(new ArticleDTO(article2));
+
+        Article article3 = new Article("제목 3", "내용 3");
+        articleList.add(new ArticleDTO(article3));
+
+        return articleList;
     }
 
     @GetMapping("/{id}")
-    public String getArticle() {
-        return "단건";
+    public ArticleDTO getArticle() {
+        Article article = new Article("제목 1", "내용 1");
+
+        ArticleDTO articleDTO = new ArticleDTO(article);
+
+        return articleDTO;
     }
 
     @PostMapping("")
-    public String create() {
+    public String create(@RequestParam("subject") String subject,
+                         @RequestParam("content") String content) {
+        System.out.println(subject);
+        System.out.println(content);
+
         return "생성";
     }
 
